c78ba0f8cf9227297358e9139b04c473dffe6d8f
Edward Lee <eddy2706@gmail.com>
2018-11-20 11:56:16 +0900

    added devise for parent and child

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :children, controllers: { registrations: 'children/registrations' }
+  devise_for :parents
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
