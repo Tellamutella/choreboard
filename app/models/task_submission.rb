06bbc0f931d4dc93efc3acd2bec81c072cc134d7
Edward Lee <eddy2706@gmail.com>
2018-11-21 18:01:21 +0900

    added cloudinary

--- app/models/task_submission.rb
+++ app/models/task_submission.rb
@@ -2,4 +2,5 @@ class TaskSubmission < ApplicationRecord
   belongs_to :child
   belongs_to :task
   belongs_to :reward_request, optional: true
+  mount_uploader :photo, PhotoUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_21_074947) do
+ActiveRecord::Schema.define(version: 2018_11_21_085219) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 2018_11_21_074947) do
     t.boolean "mandatory", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "photo"
     t.index ["child_id"], name: "index_tasks_on_child_id"
     t.index ["parent_id"], name: "index_tasks_on_parent_id"
   end
