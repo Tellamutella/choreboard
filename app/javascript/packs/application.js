5ad819a2c064821a44343319f7f23b1bf9c383a2
Edward Lee <36231356+Tellamutella@users.noreply.github.com>
2018-11-28 13:58:44 +0900

    Revert "Dashboard"

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,17 +1,1 @@
 import "bootstrap";
-
-
-let eachTask = document.querySelectorAll(".dashboard-task-name")
-eachTask.forEach(task => {
-  task.addEventListener('click', event => {
-    const allDetails = document.querySelectorAll(".task-details")
-    allDetails.forEach((detailBox) => {
-      detailBox.classList.remove("show-task");
-    });
-    const taskDetails = document.querySelector(`.task-details[data-id="${event.currentTarget.dataset.id}"]`)
-
-    taskDetails.classList.add("show-task");
-  })
-})
-
-
