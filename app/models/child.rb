723d3207dbecd9b7fbe3c6ab4147b60a8c111262
nlogx <lina.xiao@live.com>
2018-11-28 11:55:10 +0900

    updated playground events, now listener on pika and conditional html

--- app/models/child.rb
+++ app/models/child.rb
@@ -1,4 +1,5 @@
 class Child < ApplicationRecord
+  attr_reader :completed_tasks, :done_all_mandatory
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
@@ -11,4 +12,13 @@ class Child < ApplicationRecord
   has_many :reward_requests, through: :rewards
 
   validates :name, presence: true, uniqueness: true
+
+  def completed_tasks
+    tasks.count { |t| t.task_submissions.any? && t.task_submissions.last.state == 1 }
+  end
+
+  def done_all_mandatory?
+    list = tasks.select(&:mandatory)
+    list.any? && list.all? { |t| t.task_submissions.any? ? t.task_submissions.last.state == 1 : false }
+  end
 end
