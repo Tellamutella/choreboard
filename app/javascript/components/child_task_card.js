73b4b428b5a705e493ec3c6c23da5b969efa43ef
Paul Chi <paul@rocinante-media.com>
2018-11-29 11:30:56 +0900

    hidden childen tabs + get rid of that extra child

---
+++ app/javascript/components/child_task_card.js
@@ -0,0 +1,41 @@
+// window.showChildTab = function ()  {
+//   let eachChild = document.querySelectorAll(".dashboard-task-name")
+//   eachChild.forEach(child => {
+//     child.addEventListener('click', event => {
+//       // const allActive = document.querySelectorAll(".active-task")
+//       // allActive.forEach((activeTask) => {
+//       //   activeTask.classList.remove("active-task")
+//       // });
+//       // event.target.classList.add('active-task');
+//       const allDetails = document.querySelectorAll(".task-group")
+//       allDetails.forEach((detailBox) => {
+//         detailBox.classList.remove("show-task");
+//       });
+//       const taskDetails = document.querySelector(`.task-details[data-id="${event.currentTarget.dataset.id}"]`)
+
+//       taskDetails.classList.add("show-task");
+//     })
+//   })
+// }
+
+
+// const showChildTab = window.showChildTab
+
+const showChildTab = function() {
+  let childNames = document.querySelectorAll(".child-name");
+  let taskGroups = document.querySelectorAll(".task-group");
+  childNames.forEach((childName) => {
+    childName.addEventListener('click', (event) => {
+      let id = childName.dataset.id;
+      let selectedTaskGroup = taskGroups.forEach((taskGroup) => {
+        taskGroup.classList.add('hidden');
+        if (taskGroup.dataset.id === id) {
+          taskGroup.classList.remove('hidden');
+        }
+      });
+    });
+  });
+}
+
+
+export { showChildTab }
